88993a279ff54d4158cbf3b88d017adec4790ac9
Hyunsoo Kim <khs8561@naver.com>
2022-04-13 16:57:47 +0900

    photon lerp 가라앉는거 해결

--- Assets/VRAnimatorController.cs
+++ Assets/VRAnimatorController.cs
@@ -10,12 +10,15 @@ public class VRAnimatorController : MonoBehaviour
     private PhotonView pv;
    private Animator animator;
    private Vector3 previousPos;
+    private Transform tr;
    
     Vector2 primaryAxis;
     // Start is called before the first frame update
     void Start()
     {
         pv = GetComponentInParent<PhotonView>();
+        //tr = GetComponent<Transform>();
+        //Mathf.Clamp(tr.localPosition.y, 0, 999);
         animator = GetComponent<Animator>();
         Mathf.Clamp(primaryAxis.x, -1, 1);
         Mathf.Clamp(primaryAxis.y, -1, 1);
@@ -24,6 +27,11 @@ public class VRAnimatorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Mathf.Clamp(tr.localPosition.y, 0, 999)/*;*/
+        //if (tr.position.y <= -1)
+        //{
+        //    tr.position = new Vector3(tr.position.x, -0.9f,tr.position.z);
+        //}
         if (pv==null ||pv.IsMine)
         {
          primaryAxis = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
