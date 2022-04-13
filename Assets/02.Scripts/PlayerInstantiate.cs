e5963ddcf2893fc1a1c6709bdcdcb60d19f2b706
Hyunsoo Kim <khs8561@naver.com>
2022-04-13 13:08:44 +0900

    머리손핸드 onphotonserialize 싱크 시도

--- Assets/02.Scripts/PlayerInstantiate.cs
+++ Assets/02.Scripts/PlayerInstantiate.cs
@@ -6,7 +6,7 @@ using Photon.Realtime;
 using System;
 using RootMotion.FinalIK;
 
-public class PlayerInstantiate : MonoBehaviour,IPunObservable
+public class PlayerInstantiate : MonoBehaviour, IPunObservable
 {
 
 
@@ -62,7 +62,7 @@ public class PlayerInstantiate : MonoBehaviour,IPunObservable
         }
         if (!pv.IsMine)
         {
-            Destroy(vrik);
+
             controller.enabled = false;
             ovrcontroller.enabled = false;
         }
@@ -134,11 +134,23 @@ public class PlayerInstantiate : MonoBehaviour,IPunObservable
     {
         if (stream.IsWriting)
         {
-            stream.SendNext(headTr.localRotation);
+            stream.SendNext(headTr.rotation);
+            stream.SendNext(leftHandTr.rotation);
+            stream.SendNext(leftHandTr.position);
+            stream.SendNext(rightHandTr.rotation);
+            stream.SendNext(rightHandTr.position);
+
+
+
+
         }
         else
         {
             currRotHead = (Quaternion)stream.ReceiveNext();
+            curRotLeftHand = (Quaternion)stream.ReceiveNext();
+            curPosLeftHand = (Vector3)stream.ReceiveNext();
+            curRotRightHand = (Quaternion)stream.ReceiveNext();
+            curPosRightHand = (Vector3)stream.ReceiveNext();
         }
     }
     private void Update()
@@ -150,80 +162,85 @@ public class PlayerInstantiate : MonoBehaviour,IPunObservable
         else
         {
             FalseHeadRig.localRotation = currRotHead;
+            FalseLeftRig.localRotation = curRotLeftHand;
+            FalseLeftRig.localPosition = curPosLeftHand;
+            FalseRightRig.localRotation = curRotRightHand;
+            FalseRightRig.localPosition = curPosRightHand;
+
         }
     }
 }
-    //[PunRPC]
-    //private void ConnectingVRIK(string avatar)
-    //{
+//[PunRPC]
+//private void ConnectingVRIK(string avatar)
+//{
 
-    //    //print(GameObject.FindGameObjectsWithTag(avatar)[0].gameObject.transform);
-    //    vrik.solver.spine.headTarget = GameObject.FindGameObjectsWithTag(avatar)[0].gameObject.transform;
-    //    vrik.solver.leftArm.target = GameObject.FindGameObjectsWithTag(avatar)[1].gameObject.transform;
-    //    vrik.solver.rightArm.target = GameObject.FindGameObjectsWithTag(avatar)[2].gameObject.transform;
-    //}
-
-
-    //print(avatar);
-    //myavatar = GameObject.FindGameObjectWithTag(avatar);
-
-    //print(myavatar);
-    //myavatar.transform.parent = transform;
+//    //print(GameObject.FindGameObjectsWithTag(avatar)[0].gameObject.transform);
+//    vrik.solver.spine.headTarget = GameObject.FindGameObjectsWithTag(avatar)[0].gameObject.transform;
+//    vrik.solver.leftArm.target = GameObject.FindGameObjectsWithTag(avatar)[1].gameObject.transform;
+//    vrik.solver.rightArm.target = GameObject.FindGameObjectsWithTag(avatar)[2].gameObject.transform;
+//}
 
 
+//print(avatar);
+//myavatar = GameObject.FindGameObjectWithTag(avatar);
 
-
-    //}
-
-    //float rx, ry;
-    //float rotSpeed = 200;
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    // 내플레이어만
-    //    if (pv.IsMine)
-    //    {
-    //        //살았을때
-
-    //        // 마우스 입력값으로 카메라 축을 회전하고싶다.
-    //        float mx = Input.GetAxis("Mouse X");
-    //        float my = Input.GetAxis("Mouse Y");
-    //        rx += my * rotSpeed * Time.deltaTime;
-    //        ry += mx * rotSpeed * Time.deltaTime;
-
-    //        rx = Mathf.Clamp(rx, -70, 45);
-
-    //        cameraAxis.transform.eulerAngles = new Vector3(-rx, ry, 0);
-
-    //        // 키보드의 이동축을 입력받고싶다.
-    //        float h = Input.GetAxisRaw("Horizontal");
-    //        float v = Input.GetAxisRaw("Vertical");
-    //        // 키보드 입력이 있다면
-    //        if (h != 0 || v != 0)
-    //        {
-    //            // 몸을 카메라가 바라보는 앞쪽으로 회전하고싶다.
-    //            Vector3 bodyDir = cameraAxis.transform.forward;
-    //            bodyDir.y = 0;
-    //            bodyDir.Normalize();
-    //            body.forward = bodyDir;
-    //        }
-
-    //        // 몸의 앞방향을 기준으로 이동하고싶다.
-    //        Vector3 dir = body.transform.right * h + body.transform.forward * v;
-    //        dir.Normalize();
-
-
-    //        transform.position += dir * speed * Time.deltaTime;
-
-    //        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, cameraAxis.eulerAngles.y, 0));
-
-
-    //        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10);
+//print(myavatar);
+//myavatar.transform.parent = transform;
 
 
 
 
-    //    }
+//}
 
-    //}
+//float rx, ry;
+//float rotSpeed = 200;
+// Update is called once per frame
+//void Update()
+//{
+//    // 내플레이어만
+//    if (pv.IsMine)
+//    {
+//        //살았을때
+
+//        // 마우스 입력값으로 카메라 축을 회전하고싶다.
+//        float mx = Input.GetAxis("Mouse X");
+//        float my = Input.GetAxis("Mouse Y");
+//        rx += my * rotSpeed * Time.deltaTime;
+//        ry += mx * rotSpeed * Time.deltaTime;
+
+//        rx = Mathf.Clamp(rx, -70, 45);
+
+//        cameraAxis.transform.eulerAngles = new Vector3(-rx, ry, 0);
+
+//        // 키보드의 이동축을 입력받고싶다.
+//        float h = Input.GetAxisRaw("Horizontal");
+//        float v = Input.GetAxisRaw("Vertical");
+//        // 키보드 입력이 있다면
+//        if (h != 0 || v != 0)
+//        {
+//            // 몸을 카메라가 바라보는 앞쪽으로 회전하고싶다.
+//            Vector3 bodyDir = cameraAxis.transform.forward;
+//            bodyDir.y = 0;
+//            bodyDir.Normalize();
+//            body.forward = bodyDir;
+//        }
+
+//        // 몸의 앞방향을 기준으로 이동하고싶다.
+//        Vector3 dir = body.transform.right * h + body.transform.forward * v;
+//        dir.Normalize();
+
+
+//        transform.position += dir * speed * Time.deltaTime;
+
+//        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, cameraAxis.eulerAngles.y, 0));
+
+
+//        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10);
+
+
+
+
+//    }
+
+//}
 
