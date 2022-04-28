459714bc65effb31488a0716c240410092f4e723
Hyunsoo Kim <khs8561@naver.com>
2022-04-28 20:23:22 +0900

    maplf 김현수 수정

--- Assets/02.Scripts/MinigameEntrance.cs
+++ Assets/02.Scripts/MinigameEntrance.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Photon.Pun;
 
-public class MinigameEntrance : MonoBehaviour
+
+public class MinigameEntrance : MonoBehaviourPunCallbacks
 {
     private OVRManager controller;
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player") && transform.parent.name == ("MiniGame_Mole"))
+        if (other.CompareTag("Player") && transform.parent.name == ("MiniGame_Mole"))
         {
             Playdata.instance.spawnPointData = 1;
             Playdata.instance.minigameplayed[0] = true;
             PhotonNetwork.LeaveRoom();
-            SceneManager.LoadScene("Play_Game01KYJ_Wack_A_Mole");
+            //SceneManager.LoadScene("Play_Game01KYJ_Wack_A_Mole");
 
         }
         else if (other.CompareTag("Player") && transform.parent.name == ("MiniGame_Shark"))
@@ -22,20 +23,42 @@ public class MinigameEntrance : MonoBehaviour
             Playdata.instance.spawnPointData = 2;
             Playdata.instance.minigameplayed[1] = true;
             PhotonNetwork.LeaveRoom();
-            SceneManager.LoadScene("Play_Game02_KYJ_Wack_A_Shark");
+            //SceneManager.LoadScene("Play_Game02_KYJ_Wack_A_Shark");
         }
-         else if (other.CompareTag("Player") && transform.parent.name == ("MiniGame_Bird"))
+        else if (other.CompareTag("Player") && transform.parent.name == ("MiniGame_Bird"))
         {
             Playdata.instance.spawnPointData = 3;
             Playdata.instance.minigameplayed[2] = true;
             PhotonNetwork.LeaveRoom();
-            SceneManager.LoadScene("Player_Game_KYJ_003_BirdBombBowling");
+            //SceneManager.LoadScene("Player_Game_KYJ_003_BirdBombBowling");
         }
-         else if (other.CompareTag("Player") && transform.parent.name == ("MiniGame_Parachute"))
+        else if (other.CompareTag("Player") && transform.parent.name == ("MiniGame_Parachute"))
         {
             Playdata.instance.spawnPointData = 4;
             Playdata.instance.minigameplayed[3] = true;
             PhotonNetwork.LeaveRoom();
+            //SceneManager.LoadScene("Player_Game_KYJ_004_ParaglidingTown");
+        }
+
+    }
+    public override void OnLeftRoom()
+    {
+        if (Playdata.instance.spawnPointData == 1)
+        {
+            SceneManager.LoadScene("Play_Game01KYJ_Wack_A_Mole");
+        }
+        if (Playdata.instance.spawnPointData == 2)
+        {
+            SceneManager.LoadScene("Play_Game02_KYJ_Wack_A_Shark");
+
+        }
+        if (Playdata.instance.spawnPointData == 3)
+        {
+            SceneManager.LoadScene("Player_Game_KYJ_003_BirdBombBowling");
+
+        }
+        if (Playdata.instance.spawnPointData == 4)
+        {
             SceneManager.LoadScene("Player_Game_KYJ_004_ParaglidingTown");
         }
     }
